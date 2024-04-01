e2bdc9992d30baf699e94a4fed8ce6d04e959dad
KutupCevik <150686661+KutupCevik@users.noreply.github.com>
2024-04-01 22:55:25 +0200

    update homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
