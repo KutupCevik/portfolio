e74b02666a48981a301ea42617d51a8ff66cca76
KutupCevik <150686661+KutupCevik@users.noreply.github.com>
2024-04-01 22:54:53 +0200

    update app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ImprintComponent } from './legal/imprint/imprint.component';
+import { PrivacyPoliceComponent } from './legal/privacy-police/privacy-police.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HomepageComponent, FooterComponent, ImprintComponent, PrivacyPoliceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
