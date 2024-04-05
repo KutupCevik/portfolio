13e68d96d5c7ca0f341607e359eabdf1fbb9b9c8
KutupCevik <150686661+KutupCevik@users.noreply.github.com>
2024-04-06 01:51:08 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ImprintComponent } from './legal/imprint/imprint.component';
+import { PrivacyPoliceComponent } from './legal/privacy-police/privacy-police.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomepageComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy', component: PrivacyPoliceComponent },
+];
