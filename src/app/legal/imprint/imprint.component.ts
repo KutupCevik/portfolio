8f11dddcf1f204e94131aa0340fb9f42674a5d15
KutupCevik <150686661+KutupCevik@users.noreply.github.com>
2024-04-06 01:50:39 +0200

    update legal

--- src/app/legal/imprint/imprint.component.ts
+++ src/app/legal/imprint/imprint.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PrivacyPoliceComponent } from '../privacy-police/privacy-police.component';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [PrivacyPoliceComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
