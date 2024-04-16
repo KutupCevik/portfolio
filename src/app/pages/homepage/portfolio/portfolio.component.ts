743af273f545b629a6249d57916fe0c6519869c4
KutupCevik <150686661+KutupCevik@users.noreply.github.com>
2024-04-16 15:17:30 +0200

    update PortfolioComponent

--- src/app/pages/homepage/portfolio/portfolio.component.ts
+++ src/app/pages/homepage/portfolio/portfolio.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-
+  projects = [
+    {
+      name: 'Join',
+      technologie: ['Angular', 'Typescipt', 'HTML', 'CSS'],
+      img: './../../../../assets/img/projects/join.png',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      liveTest: 'https://join.kutupcevik.com/',
+      gitHub: 'https://github.com/KutupCevik/join'
+    },
+    {
+      name: 'Talon\'s Odyssey',
+      technologie: ['Angular', 'Typescipt', 'HTML', 'CSS'],
+      img: './../../../../assets/img/projects/odyssey.png',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      liveTest: 'https://odyssey.kutupcevik.com/',
+      gitHub: 'https://github.com/KutupCevik/odyssey_lost_world'
+    },
+  ]
 }
