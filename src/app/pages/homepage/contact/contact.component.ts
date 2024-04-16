37484b2c10553703e5655cc3a085b68f0efd84dd
KutupCevik <150686661+KutupCevik@users.noreply.github.com>
2024-04-16 15:18:16 +0200

    update contact

--- src/app/pages/homepage/contact/contact.component.ts
+++ src/app/pages/homepage/contact/contact.component.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule, FormControl, FormGroupDirective, Validators, FormGroup } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {MatIconModule} from '@angular/material/icon';
+import { NgClass } from '@angular/common';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatInputModule, MatFormFieldModule, ReactiveFormsModule, MatIconModule, NgClass],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
 
+export class ContactComponent {
+  matcher = new MyErrorStateMatcher();
+
+  http = inject(HttpClient);
+
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  // onSubmit(ngForm: NgForm) {
+  //   if (ngForm.valid && ngForm.submitted) {
+  //     console.log(this.contactData);
+  //   }
+  // }
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://kutupcevik.com/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      console.log(this.contactData);
+      ngForm.resetForm();
+    }
+  }
 }
