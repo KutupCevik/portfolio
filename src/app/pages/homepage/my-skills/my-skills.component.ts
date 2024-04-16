c52ada85e14f52177ae3313f90e80e747fea58bb
KutupCevik <150686661+KutupCevik@users.noreply.github.com>
2024-04-16 15:17:45 +0200

    update my-skills

--- src/app/pages/homepage/my-skills/my-skills.component.ts
+++ src/app/pages/homepage/my-skills/my-skills.component.ts
@@ -8,5 +8,46 @@ import { Component } from '@angular/core';
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
-
+  skills = [
+    {
+      name: 'Angular',
+      icon: './../../../../assets/img/icons/skills/angular.svg'
+    },
+    {
+      name: 'TypeScript',
+      icon: './../../../../assets/img/icons/skills/typescript.svg'
+    },
+    {
+      name: 'JavScript',
+      icon: './../../../../assets/img/icons/skills/javscript.svg'
+    },
+    {
+      name: 'HTML',
+      icon: './../../../../assets/img/icons/skills/html.svg'
+    },
+    {
+      name: 'Firebase',
+      icon: './../../../../assets/img/icons/skills/firebase.svg'
+    },
+    {
+      name: 'GIT',
+      icon: './../../../../assets/img/icons/skills/git.svg'
+    },
+    {
+      name: 'CSS',
+      icon: './../../../../assets/img/icons/skills/css.svg'
+    },
+    {
+      name: 'Rest-Api',
+      icon: './../../../../assets/img/icons/skills/api.svg'
+    },
+    {
+      name: 'Scrum',
+      icon: './../../../../assets/img/icons/skills/scrum.svg'
+    },
+    {
+      name: 'Material design',
+      icon: './../../../../assets/img/icons/skills/material-design.svg'
+    },
+  ];
 }
