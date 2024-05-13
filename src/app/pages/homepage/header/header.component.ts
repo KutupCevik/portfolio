4f7901a11b4b7d055f9ceef5252b5243469429b2
KutupCevik <150686661+KutupCevik@users.noreply.github.com>
2024-05-13 12:09:18 +0200

    update

--- src/app/pages/homepage/header/header.component.ts
+++ src/app/pages/homepage/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component, inject } from '@angular/core';
 import { NavbarComponent } from '../../../components/navbar/navbar.component';
 import { TranslationService } from '../../../translation.service';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavbarComponent, TranslateModule],
+  imports: [NavbarComponent, TranslateModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  constructor(public translateService: TranslateService) {
+  }
+
   translate = inject(TranslationService);
+
+  languageDE(): boolean {
+    return this.translateService.currentLang === 'de';
+  }
 }
